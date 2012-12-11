8be158e3a98909c36d3024f3bdcb81d502c3353c
nilecui <nile.cui@gmail.com>
2012-12-11 17:35:40 +0800

    Delete all plugin tools!

--- vim-bundle/bundle/hell.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#-*- coding:utf-8 -*-
-#****************************************************
-#         Author: nile cui - nile.cui@gmail.com
-#  Last modified: 2012-12-04 15:39
-#       Filename: hell.py
-#    Description:
-#****************************************************
-
